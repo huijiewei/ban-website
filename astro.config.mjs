d54ba6e1817d3e0d79dc38c545e81a91407147b7
Huijie Wei <huijiewei@outlook.com>
2024-03-27 23:45:55 +0800

    更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   site: SITE.url,
+  trailingSlash: 'never',
   integrations: [
     tailwind({
       config: {
